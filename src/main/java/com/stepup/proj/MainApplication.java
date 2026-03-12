539696050495396ffbd484933d0940181d7e15e9
Stanislav Gratskov <mihelnvkz@gmail.com>
2026-03-12 16:04:56 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,6 +5,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.out.println("1111");
+        System.out.println("1001");
+
+        System.out.println('2222');
+        System.out.println('4444');
     }
 }
