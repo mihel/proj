ef877ae821165522e164375dd1cd2f7e44990d86
Stanislav Gratskov <mihelnvkz@gmail.com>
2026-05-06 13:17:47 +0300

    Доработка логики проекта "проверка"

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -6,5 +6,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("1111");
+        System.out.println("проверка");
     }
 }
