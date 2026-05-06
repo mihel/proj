5519de8691ba0f080467aaa164df09d05de5a5df
Stanislav Gratskov <mihelnvkz@gmail.com>
2026-05-06 13:43:08 +0300

    Изменение вывода Добавлена коробка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -1,11 +1,19 @@
 package com.stepup.proj;
 
+import org.jetbrains.annotations.Contract;
+
 public class MainApplication {
 
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.out.println("1111");
+        System.out.println("1001");
         System.out.println("проверка");
+    }
+
+
+    public static void doSomething() {
+        System.out.println(1);
+
     }
 }
